b614c1be68d548808739d463ceaa0a36fe29a002
Habeebability <awoyemi.habeeb@gmail.com>
2023-08-02 11:56:35 +0100

    intial folder structure, header component, houses data

---
+++ src/main-page/header.js
@@ -0,0 +1,25 @@
+import logo from "./logo.png";
+
+const Header = ({ subtitle }) => (
+  <header className="row">
+    <div className="col-md-5">
+      <img src={logo} className="logo" alt="logo" />
+    </div>
+    <div className="col-md-7 mt-5 subtitle">{subtitle}</div>
+  </header>
+);
+
+// class Header extends Component {
+//   render() {
+//     return (
+//       <header className="row">
+//         <div className="col-md-5">
+//           <img src={logo} className="logo" alt="logo" />
+//         </div>
+//         <div className="col-md-7 mt-5 subtitle">{this.props.subtitle}</div>
+//       </header>
+//     );
+//   }
+// }
+
+export default Header;
